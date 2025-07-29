7dfd8cb7854c79d8dda116a605bc609263cbc03f
Paul Martini <paul.martini@web.de>
2025-07-29 11:24:12 +0200

    add preprocessing script for tox data

--- 00_data_preprocessing.py
+++ 00_data_preprocessing.py
@@ -1,8 +1,8 @@
 
+import os
+import numpy as np
 import scanpy as sc
 import scvelo as scv
-import numpy as np
-import os
 
 from preprocessing.data_pipeline import data_pipeline, subset_obs
 
@@ -142,11 +142,96 @@ def main_hematopoiesis():
     )
 
 
+def main_tox():
+
+    # 'sample':  {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23}
+    # 'sample name':  {'ko_liver_1', 'ko_spleen_2', 'wt_spleen_2', 'ko_liver_2', 'ko_liver_3', 'wt_spleen_1', 'wt_liver_1', 'wt_liver_3', 'ko_spleen_1', 'wt_spleen_3', 'wt_liver_2', 'ko_spleen_3'}
+    # 'subject': {0, 1, 2}, which mouse
+    # 'cluster': {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, see table in slides
+    # Tissue: {0: 'spleen', 1: 'liver'}
+    # Time: {0: 'd10', 1: 'd20'}
+    # Genotype: {0: 'ko', 1: 'wt'}
+    # Info also stored in .obs_names (e.g. wt_spleen_1_d10_id)
+
+    data_dir = './data/anndata'
+
+    subset_info = {
+        'tissue': {'spleen': 0, 'liver': 1},
+        'time': {'d10': 0, 'd20': 1},
+        'genotype': {'wt': 1, 'ko': 0},
+        'cluster': {1: 1, 2: 2}
+    }
+
+    # Define subsets ([tissue], [time], [genotype], [cluster])
+    subsets = [
+        {'tissue': ['spleen'], 'time': ['d10'], 'genotype': ['wt'], 'cluster': [1, 2]},
+        {'tissue': ['spleen'], 'time': ['d10'], 'genotype': ['ko'], 'cluster': [1, 2]}
+    ]
+
+    load_full_dataset = False
+
+    # ### Load full dataset and subset (tissue spleen, day 10)
+    if load_full_dataset:
+        toxdata_full = sc.read(os.path.join(data_dir, 'tox_ko_wt.h5ad'))
+
+        toxdata_full.raw = None  # delete the raw to avoid error when saving
+
+        # Iteratively subset the data
+        for subset in subsets:
+            mask = np.ones(len(toxdata_full), dtype=bool)
+            parts = []
+
+            for key, val in subset.items():
+                og_vals = [subset_info[key][e] for e in val]
+                mask &= toxdata_full.obs[key].isin(og_vals)
+                parts.append(f'{key}_' + ''.join(map(str, val)))
+
+            # Subset only once
+            toxdata = toxdata_full[mask, :].copy()
+            filename = 'toxdata_' + '_'.join(parts) + '.h5ad'
+
+            toxdata.write_h5ad(os.path.join(data_dir, filename))
+
+            del toxdata
+
+    for subset in subsets:
+        filename = 'toxdata_' + '_'.join(f'{key}_{"".join(map(str, val))}' for key, val in subset.items()) + '.h5ad'
+
+        print('# ### Preprocessing: ', filename, '### #')
+
+        toxdata = sc.read(os.path.join(data_dir, filename))
+
+        # Add progenitor-offspring annotations based on cluster membership
+        prog_off_dict = {'prog': [1], 'off': [2]}
+        cluster_labels = toxdata.obs['cluster'].to_numpy()
+        prog_off_anno = np.empty(toxdata.n_obs, dtype=object)
+        for key, val in prog_off_dict.items():
+            prog_off_anno[np.isin(cluster_labels, val)] = key
+        toxdata.obs['prog_off'] = prog_off_anno
+
+        # Preprocess
+        data_pipeline(
+            adata=toxdata,
+            res_path=os.path.join(data_dir, filename[:-5] + '_processed.h5ad'),
+            obs_subset_keys=None,
+            species='mus musculus',
+            pct_counts_mt_threshold=8.0,
+            cor_amb_rna=False,
+            gene_expr_threshold=(10, 'n_cells'),
+            top_k_deviant=6000,
+            diffusion_time=1,
+            verbosity=1,
+            plot=True
+        )
+
+
 if __name__ == '__main__':
 
-    main_endocrine()
+    # main_endocrine()
 
-    main_hematopoiesis()
+    # main_hematopoiesis()
+
+    # main_tox()
 
     print('done')
 
