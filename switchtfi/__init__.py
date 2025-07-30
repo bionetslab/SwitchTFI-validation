9f51026c23678b2ac12d46eeee1e0f8ec6109171
Paul Martini <paul.martini@web.de>
2025-07-30 14:42:05 +0200

    add import of data loading functions

--- switchtfi/__init__.py
+++ switchtfi/__init__.py
@@ -4,13 +4,19 @@ from .weight_fitting import calculate_weights
 from .pvalue_calculation import compute_corrected_pvalues, remove_insignificant_edges
 from .tf_ranking import rank_tfs
 from .plotting import plot_grn, plot_regulon
+from .data import (
+    preendocrine_alpha, preendocrine_beta, erythrocytes,
+    preendocrine_alpha_grn, preendocrine_beta_grn, erythrocytes_grn
+)
 
 __all__ = [
     'fit_model',
     'calculate_weights',
     'compute_corrected_pvalues', 'remove_insignificant_edges',
     'rank_tfs',
-    'plot_grn', 'plot_regulon'
+    'plot_grn', 'plot_regulon',
+    'preendocrine_alpha', 'preendocrine_beta', 'erythrocytes',
+    'preendocrine_alpha_grn', 'preendocrine_beta_grn', 'erythrocytes_grn'
 ]
 
 
