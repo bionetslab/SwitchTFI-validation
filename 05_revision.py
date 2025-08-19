95f50d4498c33741ef26e55a7545871d47e22761
Paul Martini <paul.martini@web.de>
2025-08-19 16:15:52 +0200

    refactor grn inf script

--- 05_revision.py
+++ 05_revision.py
@@ -1,3 +1,4 @@
+import os
 
 
 def main_no_imputation_deprecated():
@@ -1245,126 +1246,109 @@ def main_tcell_data_processing():
 
 def main_tcell_grn_inference():
 
+    import glob
+    import math
+
+    import pandas as pd
     import scanpy as sc
+
+    from typing import List
+    from grn_inf.grn_inference import pyscenic_pipeline, combine_grns
+
 
     # ### Define paths to files where TFs are stored
     tf_file = './data/tf/mus_musculus/allTFs_mm.txt'
 
     # ### Define paths to auxiliary annotation files needed for GRN inference with Scenic
+    db_file = (
+        './data/scenic_aux_data/databases/mouse/mm10/mc_v10_clust/'
+        'mm10_10kbp_up_10kbp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather'
+    )
+    anno_file = './data/scenic_aux_data/motif2tf_annotations/motifs-v10nr_clust-nr.mgi-m0.001-o0.0.tbl'
 
-    # ## Old (deprecated): 'mm9_mc9nr'
-    # db_file = ./data/scenic_aux_data/databases/mouse/mm9/' \
-    #           "mm9-*.mc9nr.genes_vs_motifs.rankings.feather"
-    # anno_file = ./data/scenic_aux_data/motif2tf_annotations/' \
-    #             'motifs-v9-nr.mgi-m0.001-o0.0.tbl'
+    tissues = ['spleen', 'liver']
+    time = 'd10'
+    clusters = ['345', '35']
 
-    # New (recent): 'mm10_mc_v10_clust'
-    db_file = './data/scenic_aux_data/databases/mouse/mm10/' \
-              'mc_v10_clust/mm10_10kbp_up_10kbp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather'
-    anno_file = './data/scenic_aux_data/motif2tf_annotations/' \
-                'motifs-v10nr_clust-nr.mgi-m0.001-o0.0.tbl'
-
-    data_filenames = [
-        'toxdata_tissue_spleen_time_d10_genotype_wt_cluster_12_processed.h5ad',
-        # 'toxdata_tissue_spleen_time_d10_genotype_ko_cluster_12_processed.h5ad'
-    ]
-
-    base_res_p = './results/01_grn_inf/tox'
+    base_res_p = './results/01_grn_inf/tcell'
     n_grns = 18
-    n_occurrence_threshold = 9
+    edge_count_threshold = 9
 
-    for filename in data_filenames:
-        # ### Load AnnData wist scRNA-seq data
-        toxdata = sc.read_h5ad(os.path.join('./data/anndata', filename))
+    inference = True
 
-        # ### Set parameters and perform GRN inference with Scenic
-        res_p = os.path.join(base_res_p, filename[:-5])
-        os.makedirs(res_p, exist_ok=True)
+    for tissue in tissues:
+        for cluster_keys in clusters:
 
-        gene_names = toxdata.var_names.tolist()
-        tox_genes = [v for v in gene_names if v.startswith('Tox') or v.startswith('tox')]
-        print('# ### Genes of the Tox family that are expressed in the dataset are added to the TFs:\n', tox_genes)
+            # Load data
+            id_str = f'{tissue}_{time}_{cluster_keys}'
+            filepath = os.path.join(base_res_p, f'tdata_{id_str}.h5ad')
+            tdata = sc.read_h5ad(filepath)
 
-        for i in range(n_grns):
-            print(f'# ### GRN inference, iteration {i}')
+            res_p = os.path.join(base_res_p, id_str)
+            os.makedirs(res_p, exist_ok=True)
 
-            pyscenic_pipeline(
-                adata=toxdata.copy(),
-                layer_key='scaled_log1p_norm',
-                tf_file=tf_file,
+            if inference:
+                for i in range(n_grns):
+                    print(f'# ### GRN inference, {id_str}, iteration {i}/{n_grns}')
+
+                    pyscenic_pipeline(
+                        adata=tdata.copy(),
+                        layer_key='unit_variance',
+                        tf_file=tf_file,
+                        result_folder=res_p,
+                        database_path=db_file,
+                        motif_annotations_path=anno_file,
+                        grn_inf_method='grnboost2',
+                        fn_prefix=f'{i:02d}_',
+                        verbosity=1,
+                        plot=False
+                    )
+
+            # ### Combine the 18 individual Scenic GRNs into one
+            # Edges that occur in >= n_occurrence_threshold individual GRNs are retained
+            print('### Combining Pyscenic GRNs')
+            grn_list = []
+            # Get list of paths to csv files
+            csv_files = glob.glob(res_p + '/*_pruned_grn.csv')
+            for csv_file in csv_files:
+                grn_list.append(pd.read_csv(csv_file, index_col=[0]))
+
+
+            # def combine_grns(
+            #         grns: List[pd.DataFrame],
+            # ):
+            # Todo: preserve average importance
+            #     return
+
+            combined_grn_scenic = combine_grns(
+                grn_list=grn_list,
+                n_occurrence_thresh=edge_count_threshold,
                 result_folder=res_p,
-                database_path=db_file,
-                motif_annotations_path=anno_file,
-                grn_inf_method='grnboost2',
-                fn_prefix=f'{i:02d}_tox_',
                 verbosity=1,
-                plot=False
+                fn_prefix=f'edge_count_threshold_{edge_count_threshold}_pyscenic_'
             )
 
-        # ### Combine the 18 individual Scenic GRNs into one
-        # Edges that occur in >= n_occurrence_threshold individual GRNs are retained
-        print('### Combining Pyscenic GRNs')
-        grn_list = []
-        # Get list of paths to csv files
-        csv_files = glob.glob(res_p + '/*_tox_pruned_grn.csv')
-        for csv_file in csv_files:
-            grn_list.append(pd.read_csv(csv_file, index_col=[0]))
+            # ### Combine GrnBoost2 GRNs into one (not needed afterwards)
+            print('### Combining Pyscenic GRNs')
+            grn_list = []
+            csv_files = glob.glob(res_p + '/*_basic_grn.csv')
 
-        combined_grn_scenic = combine_grns(
-            grn_list=grn_list,
-            n_occurrence_thresh=n_occurrence_threshold,
-            result_folder=res_p,
-            verbosity=1,
-            fn_prefix=f'ngrnthresh{n_occurrence_threshold}_tox_pyscenic_'
-        )
+            for csv_file in csv_files:
+                grn = pd.read_csv(csv_file, sep='\t')
+                # Extract top 1% of important edges
+                top_1_perc = math.ceil(grn.shape[0] * 0.01)
+                grn_list.append(grn[0:top_1_perc])
 
-        # ### Combine GrnBoost2 GRNs into one (not needed afterwards)
-        print('### Combining Pyscenic GRNs')
-        grn_list = []
-        csv_files = glob.glob(res_p + '/*_tox_basic_grn.csv')
-
-        for csv_file in csv_files:
-            grn = pd.read_csv(csv_file, sep='\t')
-            # Extract top 1% of important edges
-            top_1_perc = math.ceil(grn.shape[0] * 0.01)
-            grn_list.append(grn[0:top_1_perc])
-
-        combined_grn_grnboost2 = combine_grns(
-            grn_list=grn_list,
-            n_occurrence_thresh=n_occurrence_threshold,
-            result_folder=res_p,
-            verbosity=1,
-            fn_prefix=f'ngrnthresh{n_occurrence_threshold}_tox_grnboost_'
-        )
-
-        # Check whether Tox family genes appear as TFs or targets
-        res_dfs = []
-        gene_col = []
-        for tox_gene in tox_genes:
-            tfs_scenic = combined_grn_scenic['TF'].tolist()
-            n_occ_tf_scenic = sum([1 if gene == tox_gene else 0 for gene in tfs_scenic])
-            targets_scenic = combined_grn_scenic['target'].tolist()
-            n_occ_target_scenic = sum([1 if gene == tox_gene else 0 for gene in targets_scenic])
-
-            tfs_grnboost2 = combined_grn_grnboost2['TF'].tolist()
-            n_occ_tf_grnboost2 = sum([1 if gene == tox_gene else 0 for gene in tfs_grnboost2])
-            targets_grnboost2 = combined_grn_grnboost2['target'].tolist()
-            n_occ_target_grnboost2 = sum([1 if gene == tox_gene else 0 for gene in targets_grnboost2])
-
-            res_df = pd.DataFrame(
-                data=[[n_occ_tf_scenic, n_occ_tf_grnboost2], [n_occ_target_scenic, n_occ_target_grnboost2]],
-                index=['TF', 'target'],
-                columns=['SCENIC', 'GRNboost2']
+            combined_grn_grnboost2 = combine_grns(
+                grn_list=grn_list,
+                n_occurrence_thresh=edge_count_threshold,
+                result_folder=res_p,
+                verbosity=1,
+                fn_prefix=f'edge_count_threshold_{edge_count_threshold}_grnboost_'
             )
 
-            res_dfs.append(res_df)
-            gene_col.extend([tox_gene, tox_gene])
 
-            print(f'# ### Gene {tox_gene} occurs in the output GRN:\n{res_df}')
 
-        res_df = pd.concat(res_dfs)
-        res_df['Tox gene'] = gene_col
-        res_df.to_csv(os.path.join(res_p, 'tox_genes_in_grns.csv'))
 
 
 
@@ -1374,11 +1358,11 @@ if __name__ == '__main__':
 
     # main_additional_tf_target_scatter_plots()
 
-    main_tcell_data_exploration()
+    # main_tcell_data_exploration()
 
-    main_tcell_data_processing()
+    # main_tcell_data_processing()
 
-
+    main_tcell_grn_inference()
 
 
 
